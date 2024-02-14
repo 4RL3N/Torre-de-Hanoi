ba39915d394f348632f0fa2483ab724b5cb33766
4RL3N <arlen0filho04@gmail.com>
2024-02-13 21:59:44 -0300

    pequenas modificações no codigo

--- Torre_Hanoi.py
+++ Torre_Hanoi.py
@@ -2,16 +2,15 @@
 def func_torre_hanoi(n, A, C, B):
     #condicao de parada da recursao
     if n == 1:
-        print(f"Mova o disco 1 de {A} para a {C}")
+        print(f'Mova o disco 1 de {A} para a {C}')
         return
     #move os discos da torre "A" para a torre "B" utilizando a torre "C" como auxiliar
     func_torre_hanoi(n-1, A, B, C)
-    print(f"Mova o disco {n} de {A} para a {C}")
+    print(f'Mova o disco {n} de {A} para a {C}')
     #move os discos da torre "B" para a torre "C" utilizando a torre "A" como auxiliar
     func_torre_hanoi(n-1, B, C, A)
 
-print("Insira a quantidade de discos")
-n_discos = int(input())
+n_discos = int(input('Insira a quantidade de discos: '))
 
-func_torre_hanoi(n_discos, "Torre A", "Torre C", "Torre B")
-print("Concluído!")
+func_torre_hanoi(n_discos, 'Torre A', 'Torre C', 'Torre B')
+print('Concluído!')
